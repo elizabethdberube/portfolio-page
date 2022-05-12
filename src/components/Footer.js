8b8ae07155f91914872743f1c2db38ea602be102
elizabeth berube <elizabethdberube@gmail.com>
2022-05-12 13:42:16 -0400

    more css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,6 +6,7 @@ function Footer() {
         <div className='footer'>
             <a className='footer-text' href="https://www.linkedin.com/in/elizabeth-berube-b30022190">LinkedIn</a>
             <a className='footer-text' href="https://github.com/elizabethdberube">GitHub Page</a>
+            <a className='footer-text' href="https://stackexchange.com/users/25273536/elizabeth-berube">Stack Overflow</a>
         </div>
 
     );
