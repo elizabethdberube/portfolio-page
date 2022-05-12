0b414999f5d33cc71413289aa3493740fa52f42e
elizabeth berube <elizabethdberube@gmail.com>
2022-05-12 14:20:09 -0400

    Updates

--- src/utils/helpers.js
+++
@@ -1,7 +0,0 @@
-
-
-export function validateEmail(email) {
-    // eslint-disable-next-line
-    const input = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return input.test(String(email).toLowerCase());
-}
